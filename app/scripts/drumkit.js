b93a1529cad63c9d726f3d1a533f84230beeb76d
Brett <bfrable@redventures.com>
2016-12-12 07:13:52 -0500

    updated code

--- app/scripts/drumkit.js
+++ app/scripts/drumkit.js
@@ -1,18 +1,27 @@
 (function(){
     'use strict';
 
-    let drumkit = {};
-    let els = {};
-    let state = {};
+    let drumkit, els, state;
+
+    drumkit = {};
+    els = { pads: document.querySelectorAll('.pad') };
+    state = {};
+
+    drumkit.triggerKeyboardEvent = (el, keyCode, event) => {
+        var eventObj = document.createEventObject ? document.createEventObject() : document.createEvent('Events');
+
+        if(eventObj.initEvent){
+            eventObj.initEvent(event, true, true);
+        }
+
+        eventObj.keyCode = keyCode;
+        eventObj.which = keyCode;
+
+        el.dispatchEvent ? el.dispatchEvent(eventObj) : el.fireEvent(event, eventObj); // eslint-disable-line
+    };
 
     drumkit.playSound = (e) => {
         e = e || window.event;
-
-        els = {
-            audio: document.querySelector(`audio[data-key="${e.keyCode}"]`),
-            pads: document.querySelectorAll('.pad'),
-            pad: document.querySelector(`.pad[data-key="${e.keyCode}"]`)
-        };
 
         state = {
             pressed: () => {
@@ -31,12 +40,18 @@
             }
         };
 
-        els.pads.forEach(pad => pad.addEventListener('keyup', drumkit.removePressed));
-        els.pads.forEach(pad => pad.addEventListener('animationend', drumkit.removeAnimation));
+        els = {
+            audio: document.querySelector(`audio[data-key="${e.keyCode}"]`),
+            pads: document.querySelectorAll('.pad'),
+            pad: document.querySelector(`.pad[data-key="${e.keyCode}"]`)
+        };
 
         if (!els.audio) {
+            console.log('error');
             return; // stop the function from running all together
         }
+
+        els.pads.forEach(pad => pad.addEventListener('animationend', drumkit.removeAnimation));
 
         els.audio.currentTime = 0; // rewind to the start
         els.audio.play();
@@ -47,9 +62,8 @@
     };
 
     drumkit.removePressed = () => {
-        if (!els.audio) {
-            return;
-        }
+        if (!els.audio) { return; }
+
         state.depressed();
     };
 
@@ -58,8 +72,22 @@
     };
 
     drumkit.init = () => {
-        window.addEventListener('keydown', drumkit.playSound.bind(drumkit));
+        window.addEventListener('keydown', function(){
+            drumkit.playSound();
+        });
+
         window.addEventListener('keyup', drumkit.removePressed.bind(drumkit));
+
+        [].forEach.call(els.pads, function(pad) {
+            pad.addEventListener('click', function(){
+                drumkit.keyCode = this.dataset.key;
+
+                drumkit.triggerKeyboardEvent(this, drumkit.keyCode, 'keydown');
+                setTimeout(function() {
+                    drumkit.triggerKeyboardEvent(this, drumkit.keyCode, 'keyup');
+                }, 200);
+            });
+        });
     };
 
     drumkit.init();
